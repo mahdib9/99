b7d1bc2f8414ff419a411743e9a73b90b40bb987
mahdib9 <mahdib9@users.noreply.github.com>
2016-02-17 09:05:00 +0000

    Create share.lua

---
+++ plugins/share.lua
@@ -0,0 +1,19 @@
+--- https://github.com/amirhmz
+--- https://telegram.me/AmirDark
+do
+
+function run(msg, matches)
+send_contact(get_receiver(msg), "+639080376368", "Tُِele", "Blٌَُُِack", ok_cb, false)
+end
+
+return {
+patterns = {
+"^[!/]share$"
+
+},
+run = run
+}
+
+end
+--- https://github.com/amirhmz
+--- https://telegram.me/AmirDark
